0adbf8189d9252624f53a116567527c53de2cb40
JOSUE_SU <jsumare@unsa.edu.pe>
2023-12-20 10:02:22 -0500

    Created a creator of chat rooms

--- tutorial_channel/tutorial_channel/settings.py
+++ tutorial_channel/tutorial_channel/settings.py
@@ -31,15 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "daphne",
-    "django.contrib.sites",
-    "channels",
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'daphne',
+    'django.contrib.sites',
+    'channels',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'chat',
 ]
 
 MIDDLEWARE = [
